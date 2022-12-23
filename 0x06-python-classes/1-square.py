9109d67b2436df4ebae167ee00eeb8561e70470c
rachealeniola <rachealeniola92'gmail.com>
2022-12-23 22:43:40 +0000

    late

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The Module 1-square contains a class Square that defines a square:
+    private instance attribute, size
+"""
+
+
+	class Square:
+    """A Class that defines a square by size(private attribute)"""
+    def __init__(self, size=0):
+        """Initialization of the class with the size parameter."""
+        self.__size = size
+
