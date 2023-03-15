6aef0e112a6434f9b718abcf8a176df8c2c43cca
rachealeniola <rachealeniola92@gmail.com>
2023-03-15 22:31:06 +0000

    giveup

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
-t MyVar = process.argv[2];
-if (isNaN(MyVar) === true) {
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + MyVar);
+  console.log('My number:', parseInt(process.argv[2]));
 }
